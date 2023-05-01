49f708fc63f2efddf6c407af13a04470de82654d
dpatterson8583 <dpatterson8583@gmail.com>
2023-05-01 17:09:38 -0500

    Initial commit for Problematic Problem Exercise

---
+++ ProblematicProblem/BoolValueHack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblematicProblem
+{
+    internal static class BoolValueHack
+    {
+        public static bool FixBool(string s)
+        {
+            switch (s.ToLower())
+            {
+                case "yes": return true;
+              
+                case "no" : return false;
+                    
+                case "keep": return false;
+
+                case "redo": return true;
+
+                case "no thanks": return false;
+
+                case "sure": return true;
+
+                case "n": return false;
+
+                case "y": return true;
+
+                default: return false;
+
+            }
+        } 
+    }
+}
